41a7500ce57cf01d7d3b98048ad54b74b25e6f0a
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-11 14:38:39 +0200

    in progress

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 //Function to set images zoom
-function zoomImage(img) {
-    img.style.transform = "scale(2)";
-    img.style.transition = "transform 0.5s ease";
-}
+//function zoomImage(img) {
+    //img.style.transform = "scale(2)";
+    //img.style.transition = "transform 0.5s ease";
+//}
 
 //Function to set dimensions
+//function setImagesDim() {
+    //img.style.width = "60%";
+    //img.style.height = "auto";
+    //img.style.transition = "width 0.5 ease";
+//}
+
+//Function to unset dimensions
+//function unsetImagesDim() {
+    //img.style.width = "40%";
+    //img.style.height = "auto";
+    //img.style.transition = "width 0.5 ease"
+//}
