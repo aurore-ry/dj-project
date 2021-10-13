2526903ba90a27af9b1d4af831d5c97444de2ce1
aurore-ry <aremy.dev@gmail.com>
2021-10-13 17:00:27 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-<<<<<<< HEAD
 //Function to set images zoom
 //function zoomImage(img) {
-    //img.style.transform = "scale(2)";
-    //img.style.transition = "transform 0.5s ease";
+//img.style.transform = "scale(2)";
+//img.style.transition = "transform 0.5s ease";
 //}
 
 //Function to set dimensions
 //function setImagesDim() {
-    //img.style.width = "60%";
-    //img.style.height = "auto";
-    //img.style.transition = "width 0.5 ease";
+//img.style.width = "60%";
+//img.style.height = "auto";
+//img.style.transition = "width 0.5 ease";
 //}
 
 //Function to unset dimensions
 //function unsetImagesDim() {
-    //img.style.width = "40%";
-    //img.style.height = "auto";
-    //img.style.transition = "width 0.5 ease"
+//img.style.width = "40%";
+//img.style.height = "auto";
+//img.style.transition = "width 0.5 ease"
 //}
 
-const fanbaseImages = document.querySelector('.fanbase-photos');
-fanbaseImages.classList.add('animate__animated', 'animate__pulse');
-fanbaseImages.addEventListener('animationend', () => {
-    //animate fanbase 
+const fanbaseImages = document.querySelector(".fanbase-photos");
+fanbaseImages.classList.add("animate__animated", "animate__pulse");
+fanbaseImages.addEventListener("animationend", () => {
+  //animate fanbase
 });
-=======
+
 const audioPlayer = document.querySelector("audio");
->>>>>>> auroreBranch
